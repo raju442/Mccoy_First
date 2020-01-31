d530c0c6b010a68604644c8304e89e937254d7f7
raju442 <36696459+raju442@users.noreply.github.com>
2020-01-31 10:06:19 +0530

    Created class

---
+++ Git_Proj/src/Hello.java
@@ -0,0 +1,19 @@
+package com.mccoy;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Hello {
+	public static void main(String[] args) {
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
